be133fb7e754d862e89d6d06f679441deff8c45d
Isuru Perera <isuruwishwajith123@gmail.com>
2024-05-23 16:32:33 +0000

    add all

--- src/main/HelloJenkins.java
+++ src/main/HelloJenkins.java
@@ -1,7 +1,7 @@
 public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
-        System.out.println("Hello, webhook!");
+        System.out.println("Hello, Isuru!");
     }
 
     public String getGreeting() {
